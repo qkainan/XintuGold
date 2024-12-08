c3b064ee4a5370a8aaa06baefccef15b70891d9b
YUzzq <2022310220116@webmail.hzau.edu.cn>
2024-12-08 23:00:06 +0800

    feat: 样式

--- src/views/Layout/components/LayoutFooter.vue
+++ src/views/Layout/components/LayoutFooter.vue
@@ -3,26 +3,11 @@
     <!-- 联系我们 -->
     <div class="contact">
       <div class="container">
-        <dl>
-          <dt></dt>
-          <dd><i class="content"></i> 声明:以上行情仅供参考</dd>
-        </dl>
-        <dl>
-          <dt></dt>
-          <dd><i class="content"></i> 黄金定价:定金20元一克，30天内到货即可，也可以货到行情，不接受货到当天定价</dd>
-        </dl>
-        <dl>
-          <dt></dt>
-          <dd><i class="content"></i> PS:价格根据大盘时价，定价私聊确认。为避免黄金丢失，黄金单独发货，不接受与手机混包，如因跟手机混包，丢失概不负责</dd>
-        </dl>
-        <dl>
-          <dt> </dt>
-          <dd class="content">联系热线：17764039095</dd>
-        </dl>
-        <dl>
-          <dt></dt>
-          <dd><i class="content"></i> 地址:武汉市江夏区关山大道中建三局三公司住宅小区10栋107玖耀商贸</dd>
-        </dl>
+        <p>声明:以上行情仅供参考</p>
+        <p> 黄金定价:定金20元一克，30天内到货即可，也可以货到行情，不接受货到当天定价</p>
+        <p>PS:价格根据大盘时价，定价私聊确认。为避免黄金丢失，黄金单独发货，不接受与手机混包，如因跟手机混包，丢失概不负责</p>
+        <p>联系热线：17764039095</p>
+        <p>地址:武汉市江夏区关山大道中建三局三公司住宅小区10栋107玖耀商贸</p>
       </div>
     </div>
   </footer>
@@ -31,29 +16,19 @@
 <style scoped lang='scss'>
 .app_footer {
   overflow: hidden;
-  background-color:#1A1C22;
+  background-color: #1A1C22;
   padding-top: 20px;
   padding-bottom: 100px;
 
   .contact {
     margin-top: 50px;
-    background:#1A1C22;
+    background: #1A1C22;
 
-    dl {
-      height: 24px;
+    p {
       text-align: left;
       padding: 0 72px;
-      color:#ffffff;
-    }
-
-    dd {
-
+      color: #ffffff;
       font-size: 19px;
-      &:hover {
-        .iconfont {
-          color: #ffffff;
-        }
-      }
     }
   }
 }
@@ -65,18 +40,19 @@
     width: 100%;
     margin: 0 auto;
     overflow: hidden;
-    background-color:#1A1C22;
+    background-color: #1A1C22;
     padding-top: 20px;
     padding-bottom: 100px;
 
     .contact {
       margin-top: 30px;
-      background:#1A1C22;
+      background: #1A1C22;
 
 
       dd {
         text-align: start;
         font-size: 16px;
+
         &:hover {
           .iconfont {
             color: #ffffff;
